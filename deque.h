d2002d640540de590c61b7d367662c8c10ab6173
HUANG CHIA HUNG <huangchiahung@HUANGdeMacBook-Air.local>
2016-10-19 23:52:32 +0800

    define some data in classes

--- deque.h
+++ deque.h
@@ -1,22 +1,54 @@
-template<class T>
+#ifndef _DEQUE_H
+#define _DEQUE_H
+template<typename T>
 class Deque{
 public:
-  Deque();
-  bool isEmpty();
-  int size();
-  void addFirst(struct node&n);
-  void addLast(node&n);
-  T removeFirst();
-  T removeLast();
-  Iterator() p;
-  ~Deque();
+    //default constructor
+    Deque();
+    //copy constructor
+    Deque(const Deque<T> & d);
+    //assignment operator
+    Deque<T>& operator=(const Deque<T> & d);
+    //default destructor
+    ~Deque();
+    //return true if deque is empty, ie size == 0
+    inline bool isEmpty() const;
+    
+    //return the current size of deque
+    inline int size() const;
+    
+    //add a item to the front of the deque
+    void addFirst(T item);
+    
+    //add a item to the end of the deque
+    void addLast(T item);
+    
+    /* remove the first node of the deque
+     throw runtime error if trying to remove from an empty deque*/
+    T removeFirst();
+    
+    /* remove the last node of the deque
+     throw runtime error if trying to remove from an empty deque*/
+    T removeLast();
+    
+    //define iterator class
+    class Iterator;
+    
+    //return an iterator object
+    Iterator iterator() const;
+  
 private:
-  int size;
-  struct node{
-    T item;
-    struct node *next;
-    struct node *prev;
-  }
-  struct node *last;
-  struct node *first;
-} 
+    class Node;
+    //current size of the deque
+    int size;
+    //the last node
+    Node *last;
+    //the first node
+    Node *first;
+}
+
+template<typename T>
+class Deque<T>::Node{
+    
+}
+#endif
